08114a835e341bb41f449583340f52b5d0f4f90b
Julia Placzkowska <jplaczkowska6@gmail.com>
2022-04-06 09:30:57 +0200

    init spring project

---
+++ src/main/java/com/example/restdojo/RestDojoApplication.java
@@ -0,0 +1,13 @@
+package com.example.restdojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDojoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestDojoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restdojo/RestDojoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restdojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestDojoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
